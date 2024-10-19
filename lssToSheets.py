18baddc51afcd55d39be533828dc9a7af59afd97
Wonder Ginger <brandt.willems@gmail.com>
2024-10-18 21:35:17 -0700

    add printing checkpoints from best chapter

--- lssToSheets.py
+++ lssToSheets.py
@@ -45,27 +45,28 @@ def find_best_chapters(lss):
     if segments is None:
         return 1
 
-    splits = []
+    split_list = []
     subsplits = []
     golds = {}
+    best_id_dict = {}
 
     for segment in segments:
         subsplits.append(segment)
         if not segment_is_subsplit(segment.find("Name")):
-            splits.append(subsplits)
+            split_list.append(subsplits)
             subsplits = []
 
-    for subsplits in splits:
+    for split in split_list:
         split_name = ''
         best = maxsize
-        if len(subsplits) > 1:
-            name_res = re.search('^{(?P<name>.*)}',subsplits[-1].find("Name").text)
+        if len(split) > 1:
+            name_res = re.search('^{(?P<name>.*)}',split[-1].find("Name").text)
             if name_res is not None:
                 split_name = name_res.groupdict()['name']
             else:
-                split_name = subsplits[-1].find("Name").text
+                split_name = split[-1].find("Name").text
             time_dict = {}
-            for subsplit in subsplits:
+            for subsplit in split:
                 segment_history = subsplit.find("SegmentHistory")
                 for time in segment_history.findall("Time"):
                     game_time = time.find("GameTime")
@@ -74,7 +75,7 @@ def find_best_chapters(lss):
                     id = time.get("id")
                     time_str = game_time.text
 
-                    time_f = parse_time(time_str).total_seconds()
+                    time_f = round(parse_time(time_str).total_seconds(), 3)
                     if 0 == time_f:
                         print(id, time_str, game_time)
                         return 1
@@ -85,15 +86,16 @@ def find_best_chapters(lss):
                         time_dict[id] = [time_f]
             times = []
             for id in time_dict:
-                if len(time_dict[id]) == len(subsplits):
+                if len(time_dict[id]) == len(split):
                     times.append(sum(time_dict[id]))
                     sum_id = sum(time_dict[id])
                     if sum_id < best:
+                        best_id_dict[split_name] = id
                         best = sum_id
             best = timedelta(seconds = min(times))
         else:
-            split_name = subsplits[0].find("Name").text
-            best = parse_time(subsplits[0].find("BestSegmentTime").find("GameTime").text)
+            split_name = split[0].find("Name").text
+            best = parse_time(split[0].find("BestSegmentTime").find("GameTime").text)
 
         golds[split_name] = best
 
@@ -116,6 +118,78 @@ def find_best_chapters(lss):
         print_table(output_str_list)
     else:
         print('\n'.join(output_str_list))
+
+    print()
+    find_best_chapters_subsplits(split_list, best_id_dict)
+
+
+def find_best_chapters_subsplits(split_list, best_id_dict):
+    chapter_gold_subsplits = {}
+    for split in split_list:
+        if len(split) > 1:
+            name_res = re.search('^{(?P<name>.*)}',split[-1].find("Name").text)
+            if name_res is not None:
+                split_name = name_res.groupdict()['name']
+            else:
+                split_name = split[-1].find("Name").text
+            for subsplit in split:
+                subsplit_name = subsplit.find("Name").text
+                segment_history = subsplit.find("SegmentHistory")
+                for time in segment_history.findall("Time"):
+                    game_time = time.find("GameTime")
+                    if game_time is None:
+                        continue
+
+                    id = time.get("id")
+                    if best_id_dict[split_name] != id:
+                        continue
+
+                    time_str = game_time.text
+                    time_f = round(parse_time(time_str).total_seconds(), 3)
+                    if 0 == time_f:
+                        return 1
+                    ele = (subsplit_name,time_f)
+                    if split_name in chapter_gold_subsplits:
+                        chapter_gold_subsplits[split_name].append(ele)
+                    else:
+                        chapter_gold_subsplits[split_name] = [ele]
+
+    output_str_list = []
+
+    if pp:
+        output_str_list = []
+        output_list = []
+        max_len = 0
+
+        for chapter in chapter_gold_subsplits:
+            name_list = list(map(lambda x: x[0], chapter_gold_subsplits[chapter]))
+            time_list = list(map(lambda x: str(x[1]), chapter_gold_subsplits[chapter]))
+            time_list_f = list(map(lambda x: x[1], chapter_gold_subsplits[chapter]))
+
+            header_str_list = [chapter]
+            header_str_list.extend(name_list)
+
+            subsplit_str_list = [str(timedelta(seconds=sum(time_list_f)))]
+            subsplit_str_list.extend(time_list)
+
+            max_len = max(len(header_str_list), max_len)
+
+            output_list.append(header_str_list)
+            output_list.append(subsplit_str_list)
+
+        for s in output_list:
+            if len(s) < max_len:
+                s.extend([','] * (max_len - len(s) - 1))
+
+        output_str_list = list(map(lambda x: ','.join(x), output_list))
+        print_table(output_str_list, row_sep='-', row_sep_interval=2)
+
+    else:
+        for chapter in chapter_gold_subsplits:
+            output_str_list.append(chapter + ',' + ','.join(map(lambda x: str(timedelta(seconds=x[1])), chapter_gold_subsplits[chapter])))
+        print('\n'.join(output_str_list))
+
+
 
 def find_best_checkpoints(lss):
     tree = ET.parse(lss)
@@ -202,7 +276,7 @@ def find_best_split_times(lss):
             id = time.get("id")
             time_str = game_time.text
 
-            time_f = parse_time(time_str).total_seconds()
+            time_f = round(parse_time(time_str).total_seconds(), 3)
             if 0 == time_f:
                 print(id, time_str, game_time)
                 return 1
@@ -231,8 +305,6 @@ def find_best_exits(lss):
     if not isinstance(best_split_times, dict):
         return 0
 
-    # print(best_split_times)
-    # best_exits = {extract_name_from_str(k):str(timedelta(seconds=v)) for k, v in best_split_times.items() if (('cassette' in k) or (not string_is_subsplit(k)))}
     best_exits = {extract_name_from_str(k):str(timedelta(seconds=v)) for k, v in best_split_times.items() if not string_is_subsplit(k)}
     output_str_list = []
     for chapter in best_exits:
@@ -246,9 +318,7 @@ def find_best_exits(lss):
         else:
             print('\n'.join(output_str_list))
 
-        
-def print_table(rows):
-    padding = 2
+def print_table(rows, row_sep=None, row_sep_interval=1, padding=2):
     col_cnt = str.count(rows[0], ',') + 1
     max_col_w = [0] * col_cnt
     token_list_2d = []
@@ -256,18 +326,26 @@ def print_table(rows):
         tokens = str.split(line, ',')
         token_list_2d.append(tokens)
         for ii, t in enumerate(tokens):
-            max_col_w[ii] = max(len(t), max_col_w[ii])
+            max_col_w[ii] = max(len(t) + padding, max_col_w[ii])
 
+    line_count = 0
     for row in token_list_2d:
+        if (row_sep is not None) and (line_count == row_sep_interval):
+            if '\n' == row_sep:
+                print()
+            print(str(row_sep * sum(max_col_w)))
+            line_count = 0
+        line_count += 1
+
         for ii, token in enumerate(row):
             print(token , end='')
-            print(' ' * (max_col_w[ii] - len(token) + padding), end='')
+            print(' ' * (max_col_w[ii] - len(token)), end='')
         print()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('lss', nargs='*', action='store', help='path to .lss file')
+    parser.add_argument('lss', action='store', help='path to .lss file')
     parser.add_argument('-pretty', action='store_true', help='pretty print the values')
     args = parser.parse_args()
 
